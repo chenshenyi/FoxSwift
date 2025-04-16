e736f8502b5ac6ded2040b178e7e81a84947bcc6
ChenShenYi <123dtw@gmail.com>
2025-04-16 08:41:39 +0800

    chore: remove unused import in Macros.swift file

--- fox-swift-api/Sources/APICore/Macros.swift
+++ fox-swift-api/Sources/APICore/Macros.swift
@@ -5,7 +5,5 @@
 //  Created by chen shen yi on 2025/2/16.
 //
 
-import APIPlugin
-
 @attached(peer, names: suffixed(RouteCollection), suffixed(ServiceProtocol))
 public macro Service() = #externalMacro(module: "APIPlugin", type: "ServiceMacro")
